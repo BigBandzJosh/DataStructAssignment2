732c206839f18a159e08608e26635ed1e70ae274
bigbandzjosh <joosh.scott@hotmail.com>
2024-03-13 19:23:20 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -14,9 +11,6 @@ public class Main {
         head.insertEnd(8);
 
         head.printList(); // 5 2 1 3
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
     }
 }
